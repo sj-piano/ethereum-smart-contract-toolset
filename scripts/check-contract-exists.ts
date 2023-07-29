e02739fa4ab6726af0bc6720914dd15d2d1199f7
StJohn Piano <stjohnpiano@gmail.com>
2023-07-29 17:22:07 +0200

    Update scripts/check-contract-exists.ts

--- scripts/check-contract-exists.ts
+++ scripts/check-contract-exists.ts
@@ -7,13 +7,13 @@
 import { program } from "commander";
 import { ethers } from "ethers";
 import fs from "fs";
-import Joi from "joi";
 import _ from "lodash";
 
 // Local imports
 import { config } from "#root/config";
 import ethereum from "#root/src/ethereum";
 import { createLogger } from "#root/lib/logging";
+import validate from "#root/lib/validate";
 
 // Environment variables
 import dotenv from "dotenv";
@@ -21,12 +21,7 @@ import path from "path";
 let rootDir = __dirname.substring(0, __dirname.lastIndexOf("/"));
 let envFile = path.join(rootDir, config.envFileName);
 dotenv.config({ path: envFile });
-const {
-  INFURA_API_KEY_NAME,
-  LOCAL_HARDHAT_DEPLOYED_CONTRACT_ADDRESS,
-  SEPOLIA_TESTNET_DEPLOYED_CONTRACT_ADDRESS,
-  ETHEREUM_MAINNET_DEPLOYED_CONTRACT_ADDRESS,
-} = process.env;
+const { INFURA_API_KEY_NAME } = process.env;
 
 // Logging
 const { logger, log, deb } = createLogger();
@@ -40,6 +35,10 @@ program
     "specify the Ethereum network to connect to",
     "local",
   )
+  .option(
+    "--addressName <addressName>",
+    "Name of address in .env file. Overrides address or address-file.",
+  )
   .option("--address <address>", "Ethereum address.")
   .option(
     "--address-file <addressFile>",
@@ -48,51 +47,42 @@ program
 program.parse();
 const options = program.opts();
 if (options.debug) console.log(options);
-let { debug, logLevel, network: networkLabel, address, addressFile } = options;
+let {
+  debug,
+  logLevel,
+  network: networkLabel,
+  addressName,
+  address,
+  addressFile,
+} = options;
 
 // Process and validate arguments
 
-ethereum.validateAddressesSync({
-  addresses: {
-    LOCAL_HARDHAT_DEPLOYED_CONTRACT_ADDRESS,
-    SEPOLIA_TESTNET_DEPLOYED_CONTRACT_ADDRESS,
-    ETHEREUM_MAINNET_DEPLOYED_CONTRACT_ADDRESS,
-  },
-});
-
-const logLevelSchema = Joi.string().valid(...config.logLevelList);
-let logLevelResult = logLevelSchema.validate(logLevel);
-if (logLevelResult.error) {
-  var msg = `Invalid log level "${logLevel}". Valid options are: [${config.logLevelList.join(
-    ", ",
-  )}]`;
-  console.error(msg);
-  process.exit(1);
-}
+validate.logLevel({ logLevel });
 if (debug) {
   logLevel = "debug";
 }
 logger.setLevel({ logLevel });
-ethereum.setLogLevel({ logLevel });
 
-const networkLabelSchema = Joi.string().valid(...config.networkLabelList);
-let networkLabelResult = networkLabelSchema.validate(networkLabel);
-if (networkLabelResult.error) {
-  var msg = `Invalid network "${networkLabel}". Valid options are: [${config.networkLabelList.join(
-    ", ",
-  )}]`;
-  console.error(msg);
-  process.exit(1);
-}
+validate.networkLabel({ networkLabel });
 const network = config.mapNetworkLabelToNetwork[networkLabel];
 
 let contractAddress: string;
-if (address || addressFile) {
+
+if (addressName) {
+  if (!process.env[addressName]) {
+    var msg = `Address name not found in ${config.envFileName}: ${addressName}`;
+    console.error(msg);
+    process.exit(1);
+  }
+  address = process.env[addressName];
+  deb(`Address found in .env file: ${address}`);
+} else {
   if ((address && addressFile) || (!address && !addressFile)) {
-    console.error(
-      "Only one of the arguments '--address' or '--address-file' can be provided.",
-    );
-    program.help(); // Display help and exit
+    var msg =
+      "Exactly one of the arguments '--address' or '--address-file' must be provided.";
+    console.error(msg);
+    process.exit(1);
   }
   if (addressFile && !fs.existsSync(addressFile)) {
     var msg = `Address file not found: ${addressFile}`;
@@ -103,11 +93,11 @@ if (address || addressFile) {
     address = fs.readFileSync(addressFile).toString().trim();
     deb(`Address found in ${addressFile}: ${address}`);
   }
-  if (!ethers.isAddress(address)) {
-    var msg = `Invalid Ethereum address: ${address}`;
-    console.error(msg);
-    process.exit(1);
-  }
+}
+if (!ethers.isAddress(address)) {
+  var msg = `Invalid Ethereum address: ${address}`;
+  console.error(msg);
+  process.exit(1);
 }
 contractAddress = address;
 
@@ -116,33 +106,17 @@ contractAddress = address;
 let provider: ethers.Provider;
 
 var msg: string = "Unknown error";
-let DEPLOYED_CONTRACT_ADDRESS: string | undefined;
 if (networkLabel == "local") {
   msg = `Connecting to local network at ${network}...`;
   provider = new ethers.JsonRpcProvider(network);
-  DEPLOYED_CONTRACT_ADDRESS = LOCAL_HARDHAT_DEPLOYED_CONTRACT_ADDRESS;
 } else if (networkLabel == "testnet") {
   msg = `Connecting to Sepolia testnet...`;
   provider = new ethers.InfuraProvider(network, INFURA_API_KEY_NAME);
-  DEPLOYED_CONTRACT_ADDRESS = SEPOLIA_TESTNET_DEPLOYED_CONTRACT_ADDRESS;
 } else if (networkLabel == "mainnet") {
   msg = `Connecting to Ethereum mainnet...`;
   provider = new ethers.InfuraProvider(network, INFURA_API_KEY_NAME);
-  DEPLOYED_CONTRACT_ADDRESS = ETHEREUM_MAINNET_DEPLOYED_CONTRACT_ADDRESS;
 }
 log(msg);
-// A supplied argument for the contract address overrides the shell environment variable.
-if (!address) {
-  if (!DEPLOYED_CONTRACT_ADDRESS) {
-    logger.error(`No contract address supplied and no default address found.`);
-    process.exit(1);
-  }
-  contractAddress = DEPLOYED_CONTRACT_ADDRESS;
-}
-if (!ethers.isAddress(contractAddress)) {
-  logger.error(`Invalid contract address: ${contractAddress}`);
-  process.exit(1);
-}
 
 // Run main function
 
