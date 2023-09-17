4ee5149cc2cbbfa85b52020f6db73f17a8139edb
StJohn Piano <stjohnpiano@gmail.com>
2023-09-17 20:18:02 +0200

    Support functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -14,38 +14,27 @@ function getMethods(obj: any): string[] {
   const properties = new Set<string>();
   let currentObj = obj;
   do {
-    Object.getOwnPropertyNames(currentObj).forEach((item) =>
-      properties.add(item),
-    );
+    Object.getOwnPropertyNames(currentObj).forEach((item) => properties.add(item));
   } while ((currentObj = Object.getPrototypeOf(currentObj)));
-  let methods = [...properties].filter(
-    (item) => typeof obj[item] === "function",
-  );
+  let methods = [...properties].filter((item) => typeof obj[item] === "function");
   return methods.sort();
+}
+
+function isBigInt(value: any): boolean {
+  return typeof value === "bigint";
+}
+
+function isString(value: any): boolean {
+  return typeof value === "string" || value instanceof String;
+}
+
+function isNumber(value: any): boolean {
+  return typeof value === "number" && isFinite(value);
 }
 
 function isNumericString(value: string): boolean {
   value = value.trim();
   return !isNaN(value as any) && !isNaN(parseFloat(value));
-}
-
-function validateNumericString(options: {
-  name: string;
-  value: string;
-}): string {
-  const { name, value } = options;
-  const trimmedValue = value.trim();
-  if (trimmedValue.length === 0) {
-    throw new Error(`Received empty or whitespace-only string for ${name}`);
-  }
-  if (!isNumericString(trimmedValue)) {
-    let msg = `Received non-numeric string for ${name}: ${trimmedValue}`;
-    msg +=
-      `, !isNaN(value)=${!isNaN(trimmedValue as any)}, ` +
-      `!isNaN(parseFloat(value))=${!isNaN(parseFloat(trimmedValue))}`;
-    throw new Error(msg);
-  }
-  return trimmedValue;
 }
 
 const sleep = ({ seconds }: { seconds: number }) =>
@@ -55,20 +44,15 @@ function jd(obj: any): string {
   return JSON.stringify(obj, null, 2);
 }
 
-export {
-  getEnvVar,
-  getMethods,
-  isNumericString,
-  validateNumericString,
-  sleep,
-  jd,
-};
+export { getEnvVar, getMethods, isNumericString, sleep, jd };
 
 export default {
   getEnvVar,
   getMethods,
+  isBigInt,
+  isString,
+  isNumber,
   isNumericString,
-  validateNumericString,
   sleep,
   jd,
 };
--- lib/validate.ts
+++ lib/validate.ts
@@ -3,6 +3,7 @@ import Joi from "joi";
 
 // Local imports
 import { config } from "#root/config";
+import utils from "#lib/utils";
 
 // Functions
 
@@ -30,4 +31,36 @@ function networkLabel({ networkLabel }: { networkLabel: string }) {
   }
 }
 
-export default { logLevel, networkLabel };
+function numericString(options: { name: string; value: string }): string {
+  const { name, value } = options;
+  const trimmedValue = value.trim();
+  if (trimmedValue.length === 0) {
+    throw new Error(`Received empty or whitespace-only string for ${name}`);
+  }
+  if (!utils.isNumericString(trimmedValue)) {
+    let msg = `Received non-numeric string for ${name}: ${trimmedValue}`;
+    msg +=
+      `, !isNaN(value)=${!isNaN(trimmedValue as any)}, ` +
+      `!isNaN(parseFloat(value))=${!isNaN(parseFloat(trimmedValue))}`;
+    throw new Error(msg);
+  }
+  return trimmedValue;
+}
+
+function number(options: { name: string; value: number }): number {
+  const { name, value } = options;
+  if (isNaN(value)) {
+    throw new Error(`Received NaN for ${name}`);
+  }
+  return value;
+}
+
+function string(options: { name: string; value: string }): string {
+  const { name, value } = options;
+  if (!utils.isString(value)) {
+    throw new Error(`Received non-string for ${name}: ${value}`);
+  }
+  return value;
+}
+
+export default { logLevel, networkLabel, numericString, number, string };
