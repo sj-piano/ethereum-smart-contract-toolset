f2dec0ca34cdd96c5b8271ac91172ab3988b4d0a
StJohn Piano <stjohnpiano@gmail.com>
2023-10-20 21:35:50 +0100

    Test fixes and new test 'allows a user to buy tokens'

--- test/EquityToken.test.ts
+++ test/EquityToken.test.ts
@@ -23,6 +23,8 @@ import { EquityToken, EquityTokenV2 } from "../typechain-types";
 let initialSupply = 10**6;
 let decimals = 6;
 let initialSupplyInteger = BigInt(initialSupply * 10**decimals);
+let initialPrice = 1500;
+let initialPriceInteger = BigInt(initialPrice * 10**decimals);
 let admin, acc1, acc2;
 
 
@@ -42,6 +44,7 @@ describe('EquityToken walkthrough', function () {
       "EquityToken",
       "ETK",
       initialSupply,
+      initialPrice,
     ], {
       initializer: "initialize",
       kind: "uups",
@@ -92,8 +95,33 @@ describe('EquityToken walkthrough', function () {
         "EquityToken",
         "ETK",
         initialSupply,
+        initialPrice,
       )
     ).to.be.revertedWith("Initializable: contract is already initialized");
+  });
+
+  it('rejects Eth payments made directly to the contract address', async function () {
+    let equityTokenAddress = await equityToken.getAddress();
+    await expect(
+      acc1.sendTransaction({
+        to: equityTokenAddress,
+        value: ethers.parseEther("1.0"),
+      })
+    ).to.be.reverted;
+  });
+
+  it('allows a user to buy tokens', async function () {
+    const amountEth = 1;
+    const amountWei = ethers.parseEther(amountEth.toString());
+    const quantity = initialPrice / amountEth;
+    const balance1 = await equityToken.balanceOf(acc1.address);
+    await equityToken.connect(acc1).buyEquityTokens(quantity, {
+      value: amountWei,
+    });
+    const balance2 = await equityToken.balanceOf(acc1.address);
+    const balanceChange = balance2 - balance1;
+    //log(`balanceChange: ${balanceChange}`);
+    expect(balanceChange).to.equal(quantity);
   });
 
 });
@@ -147,7 +175,6 @@ describe('EquityToken - upgrade', function () {
     expect(decimals).to.equal(6);
     const decimals2 = await equityTokenV2.decimals();
     expect(decimals2).to.equal(6);
-    const version = await equityTokenV2.version();
   });
 
   it('should have an unchanged supply after upgrade', async function () {
@@ -172,6 +199,7 @@ describe('EquityToken - upgrade', function () {
         "EquityToken",
         "ETK",
         initialSupply,
+        initialPrice,
       )
     ).to.be.revertedWith("Initializable: contract is already initialized");
   });
