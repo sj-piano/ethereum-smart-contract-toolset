2a0f0560d1a63882a2a5b6354f189f64f2c06767
StJohn Piano <stjohnpiano@gmail.com>
2024-05-04 20:10:18 +0200

    Use "ethToolset"

--- test/eth-toolset.test.ts
+++ test/eth-toolset.test.ts
@@ -9,10 +9,8 @@ const helpers = require("@nomicfoundation/hardhat-network-helpers");
 
 // Local imports
 import amounts from "#src/amounts";
-import config from "#root/config";
-import ethereum from "#root/src/eth-toolset";
+import ethToolset from "#root/src/eth-toolset";
 import { createLogger } from "#root/lib/logging";
-import utils from "#root/lib/utils";
 
 
 // Controls
@@ -39,77 +37,103 @@ const networkLabel = "local";
 // Tests
 
 describe("Ethereum private key", function () {
+
+
   describe("Create random key", function () {
+
+
     it("Should be a hex string", function () {
-      const privateKey = ethereum.createPrivateKeySync();
+      const privateKey = ethToolset.createPrivateKeySync();
       const check = ethers.isHexString(privateKey);
       expect(check).to.equal(true);
     });
 
+
     it("Should be 32 bytes long", function () {
-      const privateKey = ethereum.createPrivateKeySync();
+      const privateKey = ethToolset.createPrivateKeySync();
       const check = ethers.isHexString(privateKey, 32);
       expect(check).to.equal(true);
     });
+
+
   });
 
+
   describe("Test validatePrivateKeySync", function () {
+
+
     it("Should validate a new private key", function () {
-      const privateKey = ethereum.createPrivateKeySync();
-      const check = ethereum.validatePrivateKeySync({ privateKey });
+      const privateKey = ethToolset.createPrivateKeySync();
+      const check = ethToolset.validatePrivateKeySync({ privateKey });
       expect(check).to.equal(true);
     });
 
+
     it("Should throw an error if the private key is not a hex string", function () {
       const privateKey = "0x1234567890abcdef" + "zzzz";
-      assert.throws(() => ethereum.validatePrivateKeySync({ privateKey }), Error);
+      assert.throws(() => ethToolset.validatePrivateKeySync({ privateKey }), Error);
     });
+
 
     it("Should throw an error if the private key is less than 32 bytes long", function () {
       const privateKey = "0x1234567890abcdef";
-      assert.throws(() => ethereum.validatePrivateKeySync({ privateKey }), Error);
+      assert.throws(() => ethToolset.validatePrivateKeySync({ privateKey }), Error);
     });
+
 
     it("Should throw an error if the private key is more than 32 bytes long", function () {
       const privateKey = "0x" + "1234567890abcdef".repeat(4) + "1234";
-      assert.throws(() => ethereum.validatePrivateKeySync({ privateKey }), Error);
+      assert.throws(() => ethToolset.validatePrivateKeySync({ privateKey }), Error);
     });
+
+
   });
 
+
   describe("Test deriveAddressSync", function () {
+
     it("Should derive the correct address from a private key", function () {
       const privateKey = "0x" + "00".repeat(31) + "01";
-      const address = ethereum.deriveAddressSync({ privateKey });
+      const address = ethToolset.deriveAddressSync({ privateKey });
       expect(address).to.equal(exampleAddress1);
     });
   });
 
+
   describe("Test validateAddressSync", function () {
+
+
     it("Should validate a valid address", function () {
       const address = exampleAddress1;
-      const check = ethereum.validateAddressSync({ address });
+      const check = ethToolset.validateAddressSync({ address });
       expect(check).to.equal(true);
     });
+
+
   });
 
+
   describe("Test basic transactions", function () {
+
+
     beforeEach(async function () {
       await hre.network.provider.send("hardhat_reset");
     });
 
+
     it("Should transfer 0 wei", async function () {
       const [addr1, addr2] = await ethers.getSigners();
-      let addr1Balance = await ethereum.getBalanceEth({
+      let addr1Balance = await ethToolset.getBalanceEth({
         provider,
         address: addr1.address,
       });
-      let addr2Balance = await ethereum.getBalanceEth({
+      let addr2Balance = await ethToolset.getBalanceEth({
         provider,
         address: addr2.address,
       });
       let amountWei = "0";
       let amountEth = amounts.weiToEth({ amountWei });
-      let txResponse = await ethereum.sendEth({
+      let txResponse = await ethToolset.sendEth({
         networkLabel,
         provider,
         senderAddress: addr1.address,
@@ -121,11 +145,11 @@ describe("Ethereum private key", function () {
       let txReceipt = await provider.getTransactionReceipt(txHash);
       let block = await provider.getBlock("latest");
       expect(block!.number).to.equal(1);
-      let addr1Balance2 = await ethereum.getBalanceEth({
+      let addr1Balance2 = await ethToolset.getBalanceEth({
         provider,
         address: addr1.address,
       });
-      let addr2Balance2 = await ethereum.getBalanceEth({
+      let addr2Balance2 = await ethToolset.getBalanceEth({
         provider,
         address: addr2.address,
       });
@@ -147,12 +171,12 @@ describe("Ethereum private key", function () {
 
       const [addr1, addr2] = await ethers.getSigners();
 
-      let addr1Balance = await ethereum.getBalanceEth({
+      let addr1Balance = await ethToolset.getBalanceEth({
         provider,
         address: addr1.address,
       });
       //log("addr1Balance", addr1Balance);
-      let addr2Balance = await ethereum.getBalanceEth({
+      let addr2Balance = await ethToolset.getBalanceEth({
         provider,
         address: addr2.address,
       });
@@ -161,7 +185,7 @@ describe("Ethereum private key", function () {
       let amountWei = "1";
       let amountEth = amounts.weiToEth({ amountWei });
 
-      let txResponse = await ethereum.sendEth({
+      let txResponse = await ethToolset.sendEth({
         networkLabel,
         provider,
         senderAddress: addr1.address,
@@ -174,17 +198,17 @@ describe("Ethereum private key", function () {
       let requiredConfirms = 12;
       let confirms;
 
-      //confirms = await ethereum.getTxConfirms({ provider, txHash });
+      //confirms = await ethToolset.getTxConfirms({ provider, txHash });
       //log("Tx confirms", confirms);
 
       await helpers.mine(10);
 
-      //confirms = await ethereum.getTxConfirms({ provider, txHash });
+      //confirms = await ethToolset.getTxConfirms({ provider, txHash });
       //log("Tx confirms", confirms);
 
       await helpers.mine(2);
 
-      confirms = await ethereum.getTxConfirms({ provider, txHash });
+      confirms = await ethToolset.getTxConfirms({ provider, txHash });
       //log("Tx confirms", confirms);
 
       expect(confirms).to.equal(requiredConfirms);
@@ -192,13 +216,13 @@ describe("Ethereum private key", function () {
       //log("addr1.address", addr1.address);
       //log("addr2.address", addr2.address);
 
-      let addr1Balance2 = await ethereum.getBalanceEth({
+      let addr1Balance2 = await ethToolset.getBalanceEth({
         provider,
         address: addr1.address,
       });
       //log("addr1Balance2", addr1Balance2);
 
-      let addr2Balance2 = await ethereum.getBalanceEth({
+      let addr2Balance2 = await ethToolset.getBalanceEth({
         provider,
         address: addr2.address,
       });
@@ -207,7 +231,7 @@ describe("Ethereum private key", function () {
       let differenceWei = ethers.parseEther(addr2Balance2) - ethers.parseEther(addr2Balance);
       expect(differenceWei).to.equal(1);
 
-      let { txFeeWei, txFeeEth } = await ethereum.getTxFees({
+      let { txFeeWei, txFeeEth } = await ethToolset.getTxFees({
         provider,
         txHash,
       });
